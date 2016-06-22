f7203b7f03f64c6e8133355a7f4c537742bd6119
Mitesh S <Mitesh S>
2016-06-22 12:54:38 -0400

    change metadata

---
+++ iis/attributes/default.rb
@@ -0,0 +1,7 @@
+default["iis"]["doc_root"] = "C:/inetpub/wwwroot"
+default["iis"]["features"] = [
+  "Web-Server",
+  "Web-Http-Tracing",
+  "Web-Http-Redirect",
+  "Web-Mgmt-Console",
+]
---
+++ iis/metadata.rb
@@ -0,0 +1,7 @@
+name        "iis"
+description "Install & starts IIS"
+license     "Apache 2.0"
+version     "0.0.1"
+
+supports    "windows"
+
